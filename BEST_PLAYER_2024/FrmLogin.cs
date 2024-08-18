dbc933539ba2b9d6ab25d8025cc94ed6667b593f
elcachetom <elcachetom2005@gmail.com>
2024-08-17 21:21:15 -0600

    FrmLogin Boton close system

--- BEST_PLAYER_2024/FrmLogin.cs
+++ BEST_PLAYER_2024/FrmLogin.cs
@@ -16,5 +16,10 @@ namespace BEST_PLAYER_2024.Resources
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
