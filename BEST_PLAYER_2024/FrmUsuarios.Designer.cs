1eaaf3c621eaad107b8de9292a9b440859276087
elcachetom <elcachetom2005@gmail.com>
2024-08-17 23:11:54 -0600

    Probando Abrir Formulario hijo2 dentro de formlario hijo1 en un mismo panel
    Probando Abrir Formulario hijo2 dentro de formlario hijo1 en un mismo panel

--- BEST_PLAYER_2024/FrmUsuarios.Designer.cs
+++ BEST_PLAYER_2024/FrmUsuarios.Designer.cs
@@ -30,6 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmUsuarios));
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.PnlHijoContendraHijo2 = new System.Windows.Forms.Panel();
+            this.PnlHijoContendraHijo2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -37,20 +40,37 @@
             resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
             // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PnlHijoContendraHijo2
+            // 
+            this.PnlHijoContendraHijo2.Controls.Add(this.label1);
+            this.PnlHijoContendraHijo2.Controls.Add(this.button1);
+            resources.ApplyResources(this.PnlHijoContendraHijo2, "PnlHijoContendraHijo2");
+            this.PnlHijoContendraHijo2.Name = "PnlHijoContendraHijo2";
+            // 
             // FrmUsuarios
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PnlHijoContendraHijo2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FrmUsuarios";
+            this.PnlHijoContendraHijo2.ResumeLayout(false);
+            this.PnlHijoContendraHijo2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel PnlHijoContendraHijo2;
     }
 }
