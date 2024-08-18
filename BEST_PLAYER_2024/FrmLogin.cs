83d39ceedf4bbe04423696a91ac6702ae6658fbb
elcachetom <elcachetom2005@gmail.com>
2024-08-17 22:53:26 -0600

    Creacion de abrir Frm en un panel, evento de movimeinto de formulario en la ventana
    Creación d movimiento de un formulario en la pantalla y creación de funcionalidad d un formulario abierto dentro de un panel también interacción entre formularios con hide
    VERIFICAR PORQUE NO SIRVE .CLOSE()

--- BEST_PLAYER_2024/FrmLogin.cs
+++ BEST_PLAYER_2024/FrmLogin.cs
@@ -1,26 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BEST_PLAYER_2024.Resources
 {
     public partial class FrmLogin : Form
     {
+        // Declarar variables para el movimiento del formulario
+        private bool isDragging = false;
+        private Point lastCursor;
+        private Point lastForm;
+
         public FrmLogin()
         {
             InitializeComponent();
+
+            this.MouseDown += new MouseEventHandler(FrmLogin_MouseDown);
+            this.MouseMove += new MouseEventHandler(FrmLogin_MouseMove);
+            this.MouseUp += new MouseEventHandler(FrmLogin_MouseUp);
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            //Agrengando codigo para cerrar la ventana
-            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Crear una instancia de FrmDashboard
+            FrmDashboard frmDashboard = new FrmDashboard();
+
+            // Mostrar FrmDashboard
+            frmDashboard.Show();
+
+            // Ocultar el formulario actual
+            this.Hide();
+        }
+
+        private void FrmLogin_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Iniciar el movimiento cuando el botón izquierdo del mouse se presiona
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = true;
+                lastCursor = Cursor.Position;
+                lastForm = this.Location;
+            }
+        }
+
+        private void FrmLogin_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Mover el formulario cuando se está arrastrando
+            if (isDragging)
+            {
+                Point delta = Point.Subtract(Cursor.Position, new Size(lastCursor));
+                this.Location = Point.Add(lastForm, new Size(delta));
+            }
+        }
+
+        private void FrmLogin_MouseUp(object sender, MouseEventArgs e)
+        {
+            // Detener el movimiento cuando el botón del mouse se suelta
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = false;
+            }
         }
     }
 }
