be2d39f7de54772062093d7db5b7ef06ab43de27
elcachetom <elcachetom2005@gmail.com>
2024-08-17 21:55:59 -0600

    FrmLogin y FrmDshboard
    Agregar botom radio en el login y agregado de botom dashboard

--- BEST_PLAYER_2024/FrmDashboard.Designer.cs
+++ BEST_PLAYER_2024/FrmDashboard.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.rjButton1 = new RJCodeAdvance.RJControls.RJButton();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // rjButton1
@@ -59,11 +60,21 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(511, 90);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 89);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // FrmDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.rjButton1);
             this.Name = "FrmDashboard";
@@ -76,5 +87,6 @@
 
         private RJCodeAdvance.RJControls.RJButton rjButton1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
