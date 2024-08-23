6389418106d257059b84a97e053469db4d9b7cab
alejo-gg <GG241648@alumno.udb.edu.sv>
2024-08-23 17:15:43 -0600

    FrmTablaVotaciones y FrmEquipos
    Creación del formulario tablavotacion y equipos

---
+++ BEST_PLAYER_2024/FrmTablaVotaciones.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace BEST_PLAYER_2024
+{
+    partial class FrmTablaVotaciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.DgvVotaciones = new System.Windows.Forms.DataGridView();
+            this.BtnRegresar = new System.Windows.Forms.Label();
+            this.BtnDescargar = new System.Windows.Forms.Button();
+            this.ColTop = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColEquipo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColVotos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvVotaciones)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DgvVotaciones
+            // 
+            this.DgvVotaciones.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DgvVotaciones.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.DgvVotaciones.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.DgvVotaciones.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.DgvVotaciones.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(113)))), ((int)(((byte)(62)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(232)))), ((int)(((byte)(120)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(52)))), ((int)(((byte)(28)))));
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DgvVotaciones.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.DgvVotaciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvVotaciones.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColTop,
+            this.ColNombre,
+            this.ColEquipo,
+            this.ColVotos});
+            this.DgvVotaciones.EnableHeadersVisualStyles = false;
+            this.DgvVotaciones.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(113)))), ((int)(((byte)(62)))));
+            this.DgvVotaciones.Location = new System.Drawing.Point(14, 95);
+            this.DgvVotaciones.Margin = new System.Windows.Forms.Padding(5);
+            this.DgvVotaciones.Name = "DgvVotaciones";
+            this.DgvVotaciones.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(69)))), ((int)(((byte)(69)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(232)))), ((int)(((byte)(120)))));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DgvVotaciones.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(69)))), ((int)(((byte)(69)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(232)))), ((int)(((byte)(120)))));
+            this.DgvVotaciones.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.DgvVotaciones.Size = new System.Drawing.Size(831, 489);
+            this.DgvVotaciones.TabIndex = 37;
+            // 
+            // BtnRegresar
+            // 
+            this.BtnRegresar.AutoSize = true;
+            this.BtnRegresar.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnRegresar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(232)))), ((int)(((byte)(120)))));
+            this.BtnRegresar.Location = new System.Drawing.Point(766, 40);
+            this.BtnRegresar.Name = "BtnRegresar";
+            this.BtnRegresar.Size = new System.Drawing.Size(82, 21);
+            this.BtnRegresar.TabIndex = 41;
+            this.BtnRegresar.Text = "Regresar";
+            // 
+            // BtnDescargar
+            // 
+            this.BtnDescargar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(232)))), ((int)(((byte)(120)))));
+            this.BtnDescargar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnDescargar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnDescargar.Font = new System.Drawing.Font("Century Gothic", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnDescargar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(52)))), ((int)(((byte)(28)))));
+            this.BtnDescargar.Location = new System.Drawing.Point(47, 25);
+            this.BtnDescargar.Name = "BtnDescargar";
+            this.BtnDescargar.Size = new System.Drawing.Size(187, 36);
+            this.BtnDescargar.TabIndex = 42;
+            this.BtnDescargar.Text = "Descargar tabla";
+            this.BtnDescargar.UseVisualStyleBackColor = false;
+            // 
+            // ColTop
+            // 
+            this.ColTop.FillWeight = 18.27411F;
+            this.ColTop.HeaderText = "Top";
+            this.ColTop.Name = "ColTop";
+            // 
+            // ColNombre
+            // 
+            this.ColNombre.FillWeight = 18.27411F;
+            this.ColNombre.HeaderText = "Nombre";
+            this.ColNombre.Name = "ColNombre";
+            // 
+            // ColEquipo
+            // 
+            this.ColEquipo.FillWeight = 18.27411F;
+            this.ColEquipo.HeaderText = "Equipo";
+            this.ColEquipo.Name = "ColEquipo";
+            // 
+            // ColVotos
+            // 
+            this.ColVotos.FillWeight = 18.27411F;
+            this.ColVotos.HeaderText = "Votos";
+            this.ColVotos.Name = "ColVotos";
+            // 
+            // FrmTablaVotaciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(21)))), ((int)(((byte)(21)))));
+            this.ClientSize = new System.Drawing.Size(872, 611);
+            this.Controls.Add(this.BtnDescargar);
+            this.Controls.Add(this.BtnRegresar);
+            this.Controls.Add(this.DgvVotaciones);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmTablaVotaciones";
+            this.Text = "FrmTablaVotaciones";
+            ((System.ComponentModel.ISupportInitialize)(this.DgvVotaciones)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DgvVotaciones;
+        private System.Windows.Forms.Label BtnRegresar;
+        private System.Windows.Forms.Button BtnDescargar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColTop;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColEquipo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColVotos;
+    }
+}
