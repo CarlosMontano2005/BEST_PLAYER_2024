55a0af5dbd8def1a204abb8cff19bf692472f8d9
Ander45Portafol <anderdeco25@gmail.com>
2024-10-09 19:56:25 -0600

    Tabla jugadores
    Cargando datos en tabla de jugadores y mostrando la informacion detallada de cada jugador

---
+++ Servicios/ServJugador.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+using Modelo;
+
+
+namespace Servicios
+{
+    public class ServJugador
+    {
+        public static DataTable cargarDatos(int idjugador) {
+            DataTable datos = ModeloJugador.cargarJugador(out string message,idjugador);
+            return datos;
+        }
+        public static DataTable CargarJugadores() {
+            DataTable data = ModeloJugador.cargarJugadores(out string message);
+            return data;
+        }
+    }
+}
