aac7bdf0761174d96b1633f3951dfd5f620e0626
elcachetom <elcachetom2005@gmail.com>
2024-10-23 08:33:37 -0600

    Votacion Fotos en los top
    Votacion Fotos en los top

--- BEST_PLAYER_2024/FrmTopJugadores.cs
+++ BEST_PLAYER_2024/FrmTopJugadores.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data;
 using Servicios;
+using System.IO;
 
 namespace BEST_PLAYER_2024
 {
@@ -24,47 +25,54 @@ namespace BEST_PLAYER_2024
             DataTable datos = ServTopJugadores.CargarTopFiveJugadores();
             try
             {
-                if (datos.Rows.Count > 0)
+                for (int i = 0; i < datos.Rows.Count && i < 5; i++)
                 {
-                    lblCantVotos_1.Text = datos.Rows[0]["cantidadVotos"].ToString();
-                    string nombre = datos.Rows[0]["Nombre"].ToString();
-                    string apellido = datos.Rows[0]["Apellido"].ToString();
-                    lblNombreTop1.Text = nombre + " " + apellido;
-                    lblEquipoTop1.Text = datos.Rows[0]["NombreEquipo"].ToString();
-                }
-                if (datos.Rows.Count > 1)
-                {
-                    lblCantVotos_2.Text = datos.Rows[1]["cantidadVotos"].ToString();
-                    string nombre = datos.Rows[1]["Nombre"].ToString();
-                    string apellido = datos.Rows[1]["Apellido"].ToString();
-                    lblNombreTop2.Text = nombre + " " + apellido; lblEquipoTop2.Text = datos.Rows[1]["NombreEquipo"].ToString();
-                }
+                    Label lblCantVotos = this.Controls.Find($"lblCantVotos_{i + 1}", true).FirstOrDefault() as Label;
+                    Label lblNombreTop = this.Controls.Find($"lblNombreTop{i + 1}", true).FirstOrDefault() as Label;
+                    Label lblEquipoTop = this.Controls.Find($"lblEquipoTop{i + 1}", true).FirstOrDefault() as Label;
+                    if (lblCantVotos != null)
+                        lblCantVotos.Text = datos.Rows[i]["cantidadVotos"].ToString();
+                    if (lblNombreTop != null && lblEquipoTop != null)
+                    {
+                        string nombre = datos.Rows[i]["Nombre"].ToString();
+                        string apellido = datos.Rows[i]["Apellido"].ToString();
+                        lblNombreTop.Text = $"{nombre} {apellido}";
+                        lblEquipoTop.Text = datos.Rows[i]["NombreEquipo"].ToString();
+                    }
+                    PictureBox pxFotoJugador = this.Controls.Find($"PxFotoJugador_{i + 1}", true).FirstOrDefault() as PictureBox;
+                    PictureBox pxbLogoEq = this.Controls.Find($"PxbLogoEq_{i + 1}", true).FirstOrDefault() as PictureBox;
 
-                if (datos.Rows.Count > 2)
-                {
-                    lblCantVotos_3.Text = datos.Rows[2]["cantidadVotos"].ToString();
-                    string nombre = datos.Rows[2]["Nombre"].ToString();
-                    string apellido = datos.Rows[2]["Apellido"].ToString();
-                    lblNombreTop3.Text = nombre + " " + apellido;
-                    lblEquipoTop3.Text = datos.Rows[2]["NombreEquipo"].ToString();
-                }
+                    if (pxFotoJugador != null)
+                    {
+                        byte[] imagenBytesJugador = datos.Rows[i]["Foto"] as byte[];
+                        if (imagenBytesJugador != null && imagenBytesJugador.Length > 0)
+                        {
+                            using (MemoryStream ms = new MemoryStream(imagenBytesJugador))
+                            {
+                                pxFotoJugador.Image = new Bitmap(ms);
+                            }
+                        }
+                    }
 
-                if (datos.Rows.Count > 3)
-                {
-                    lblCantVotos_4.Text = datos.Rows[3]["cantidadVotos"].ToString();
-                    string nombre = datos.Rows[3]["Nombre"].ToString();
-                    string apellido = datos.Rows[3]["Apellido"].ToString();
-                    lblNombreTop4.Text = nombre + " " + apellido; lblEquipoTop4.Text = datos.Rows[3]["NombreEquipo"].ToString();
+                    if (pxbLogoEq != null)
+                    {
+                        byte[] imagenBytesLogo = datos.Rows[i]["FotoLogo"] as byte[];
+                        if (imagenBytesLogo != null && imagenBytesLogo.Length > 0)
+                        {
+                            using (MemoryStream ms = new MemoryStream(imagenBytesLogo))
+                            {
+                                pxbLogoEq.Image = new Bitmap(ms);
+                            }
+                        }
+                    }
                 }
-
             }
             catch (Exception ex)
             {
-
                 MessageBox.Show($"Error al cargar los datos: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-          
         }
+
         private void BtnBuscar_Click(object sender, EventArgs e)
         {
             FrmTablaVotaciones child2 = new FrmTablaVotaciones();
